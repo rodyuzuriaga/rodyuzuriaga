eb498fae05952d5e1adeb5dcb80e7ae0732ca69d
Rody Uzuriaga <92474542+rodyuzuriaga@users.noreply.github.com>
2025-08-22 01:55:08 -0500

    Initial Commit

--- update_badges.py
+++ update_badges.py
@@ -38,11 +38,15 @@ def generate_badges_html(badges):
     if not badges:
         return "<div align='center'>No se encontraron badges</div>"
 
-    html = '<div style="display:flex; flex-wrap:wrap; justify-content:center; gap:10px;">\n'
-    for b in badges:
-        html += f'  <a href="{b["link"]}" target="_blank" title="{b["name"]}" style="text-align:center; flex: 0 0 calc(100% / {BADGES_PER_ROW} - 10px); box-sizing:border-box;">'
-        html += f'<img src="{b["img"]}" alt="{b["name"]}" style="height:100px; width:auto; display:block; margin:auto;"></a>\n'
-    html += '</div>'
+    html = ''
+    for i in range(0, len(badges), BADGES_PER_ROW):
+        row_badges = badges[i:i+BADGES_PER_ROW]
+        html += '<div style="display:flex; justify-content:center; gap:10px; margin-bottom:10px;">\n'
+        for b in row_badges:
+            html += f'  <a href="{b["link"]}" target="_blank" title="{b["name"]}" style="text-align:center;">'
+            html += f'<img src="{b["img"]}" alt="{b["name"]}" style="height:100px; width:auto; display:block; margin:auto;"></a>\n'
+        html += '</div>\n'
+
     return html
 
 def update_readme(html):
