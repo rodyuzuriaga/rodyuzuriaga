97a700fea0cadf3424c393dfd95bed38bbdaa49b
Rody Uzuriaga <92474542+rodyuzuriaga@users.noreply.github.com>
2025-08-21 23:30:49 -0500

    Initial Commit

--- update_badges.py
+++ update_badges.py
@@ -1,6 +1,5 @@
 import requests
 import re
-from math import ceil
 
 url = "https://www.credly.com/users/rody-angel-uzuriaga-aviles/badges.json?page=1"
 headers = {"User-Agent": "Mozilla/5.0"}
@@ -14,25 +13,16 @@ for item in data.get("data", []):
     img = attributes.get("image_url", item.get("earner_photo_url", ""))
     name = attributes.get("name", "Badge")
     if link and img:
-        badges.append(f"[![{name}]({img})]({link})<br>{name}")
+        badges.append(f"[![{name}]({img})]({link})")
 
-columns = 3
-rows = ceil(len(badges) / columns)
-table_rows = []
-
-for r in range(rows):
-    row = badges[r*columns:(r+1)*columns]
-    table_rows.append("| " + " | ".join(row) + " |")
-
-separator = "| " + " | ".join(["---"]*columns) + " |"
-table_md = "\n".join([table_rows[0], separator] + table_rows[1:]) if badges else "No se encontraron insignias en Credly"
+badges_md = " ".join(badges) if badges else "No se encontraron insignias en Credly"
 
 with open("README.md", "r", encoding="utf-8") as f:
     content = f.read()
 
 new_content = re.sub(
     r"<!--START_SECTION:badges-->.*<!--END_SECTION:badges-->",
-    f"<!--START_SECTION:badges-->\n{table_md}\n<!--END_SECTION:badges-->",
+    f"<!--START_SECTION:badges-->\n{badges_md}\n<!--END_SECTION:badges-->",
     content,
     flags=re.S
 )
