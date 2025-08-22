070968c694a857f58177a10d8d9274abea3d53d0
Rody Uzuriaga <92474542+rodyuzuriaga@users.noreply.github.com>
2025-08-22 00:37:59 -0500

    Initial Commit

--- update_badges.py
+++ update_badges.py
@@ -2,26 +2,18 @@ import requests
 import re
 
 url = "https://www.credly.com/users/rody-angel-uzuriaga-aviles/badges.json?page=1"
-
-headers = {
-    "User-Agent": "Mozilla/5.0"
-}
+headers = {"User-Agent": "Mozilla/5.0"}
 resp = requests.get(url, headers=headers)
 data = resp.json()
 
 badges = []
 for item in data.get("data", []):
-    attributes = item.get("badge_template", {})
-    link = attributes.get("global_activity_url", "")
-    img = attributes.get("image_url", item.get("image_url", ""))
-    name = attributes.get("name", "Badge")
-    if link and img:
-        badges.append(f'<a href="{link}"><img src="{img}" alt="{name}" height="100"/></a>')
+    template = item.get("badge_template", {})
+    img = template.get("image_url")
+    if img:
+        badges.append(f'<img src="{img}" alt="badge" height="100" style="margin:5px"/>')
 
-if not badges:
-    badges_html = "<p>No se encontraron insignias en Credly</p>"
-else:
-    badges_html = "\n".join(badges)
+badges_html = f'<div align="center">{"".join(badges)}</div>' if badges else "<p>No se encontraron insignias</p>"
 
 with open("README.md", "r", encoding="utf-8") as f:
     content = f.read()
